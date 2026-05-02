d5b0b60350c0ed816c32b6e12de683f582e65c47
Mathieu Escande <mathieu.escande@gmail.com>
2026-05-02 08:09:49 +0200

    voice

---
+++ .claude/run-dev.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export PATH="/usr/local/bin:$PATH"
+cd /Users/mathieuescande/Documents/GitHub/sacredpath-mainstream
+/usr/local/bin/node /usr/local/bin/../lib/node_modules/npm/bin/npm-cli.js run dev
